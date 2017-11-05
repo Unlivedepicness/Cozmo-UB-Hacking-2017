702733c13874149bbeb198b2ecf91863f562d77c
Miles <31706085+milesave@users.noreply.github.com>
2017-11-05 07:55:58 -0500

    Weather test for cozmo integration

---
+++ weather_test.py
@@ -0,0 +1,22 @@
+from weather import *
+
+weather = Weather()
+
+loc = 'buffalo'
+
+location = weather.lookup_by_location(loc)
+condition = location.condition()
+
+print("currently " + condition['text'])
+
+forecasts = location.forecast()
+
+print("today in " + loc + " the weather is")
+print(forecasts[0].text())
+print("with a high of " + forecasts[0].high())
+print("and a low of " + forecasts[0].low())
+
+print("tomorrow the weather is")
+print(forecasts[1].text())
+print("with a high of " + forecasts[1].high())
+print("and a low of " + forecasts[1].low())
